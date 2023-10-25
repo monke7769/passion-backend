d9d290a96d73d8f98269a8fd7ed3abd0ece8a4ab
Shuban <67974101+shuban-789@users.noreply.github.com>
2023-10-25 10:31:29 -0700

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -5,99 +5,96 @@
 
 # DISCLAIMER: This implementations follows RSA and is not a custom algorithm. And neither is the miller-rabin test.
 
-
 import random
-import math
 
-# Test whether or not number is prime
-class rsa:
-def is_prime(n, k=5):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    # Miler-Rabin primality test algorithm
-    def miller_rabin(n, d):
-        a = random.randint(2, n - 2)
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            return True
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                return True
-        return False
+class RSA:
+    def __init__(self, bits=2048):
+        self.bits = bits
+        self.public_key, self.private_key = self.rsa_keygen(bits)
 
-  
-    # Compute r = n - 1
-    r, d = 0, n - 1
-    while d % 2 == 0:
-        r += 1
-        d //= 2
-
-    # If it does not pass the miller rabin test, it is not prime. Else it is prime
-    for _ in range(k):
-        if not miller_rabin(n, d):
+    @staticmethod
+    def is_prime(n, k=5):
+        if n <= 1:
             return False
-    return True
+        if n <= 3:
+            return True
+        # Miler-Rabin primality test algorithm
+        def miller_rabin(n, d):
+            a = random.randint(2, n - 2)
+            x = pow(a, d, n)
+            if x == 1 or x == n - 1:
+                return True
+            for _ in range(k - 1):  # Fix variable name 'k'
+                x = pow(x, 2, n)
+                if x == n - 1:
+                    return True
+            return False
 
-# Generate RSA bits (if p is a prime number)
-def generate_prime(bits):
-    while True:
-        p = random.getrandbits(bits)
-        if p % 2 == 0:
-            p += 1
-        if is_prime(p):
-            return p
+        # Compute r = n - 1
+        r, d = 0, n - 1
+        while d % 2 == 0:
+            r += 1
+            d //= 2
+        # If it does not pass the miller rabin test, it is not prime
+        for _ in range(k):
+            if not miller_rabin(n, d):
+                return False
+        return True
 
-# Modular inverse must exist for decryption
-def mod_inverse(a, m):
-    g, x, y = extended_gcd(a, m)
-    if g != 1:
-        raise ValueError("DNE: Modular inverse does not exist")
-    return x % m
+    @staticmethod
+    def generate_prime(bits):
+        while True:
+            p = random.getrandbits(bits)
+            if p % 2 == 0:
+                p += 1
+            if RSA.is_prime(p):  # If it is a prime number, return it
+                return p
 
-def extended_gcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, x, y = extended_gcd(b % a, a)
-        return (g, y - (b // a) * x, x)
+    @staticmethod
+    def mod_inverse(a, m):
+        g, x, y = RSA.extended_gcd(a, m)  # Might be slow
+        if g != 1:
+            raise ValueError("DNE: Modular inverse does not exist")
+        return x % m
 
-# Main rsa keygen algorithm
-def rsa_keygen(bits=2048):
-    p = generate_prime(bits)
-    q = generate_prime(bits)
-    n = p * q
-    phi = (p - 1) * (q - 1)
-    e = 65537
-    d = mod_inverse(e, phi)
-    return (n, e), (n, d)
+    @staticmethod
+    def extended_gcd(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, x, y = RSA.extended_gcd(b % a, a)  # Call extended_gcd as a static method
+            return (g, y - (b // a) * x, x)
 
-# Encrypt with public key
-def rsa_encrypt(public_key, plaintext):
-    n, e = public_key
-    cipher = pow(plaintext, e, n)
-    return cipher
+    def rsa_keygen(self, bits):
+        p = RSA.generate_prime(bits)  # Let P and Q be random prime numbers
+        q = RSA.generate_prime(bits)  
+        n = p * q
+        phi = (p - 1) * (q - 1)
+        e = 65537
+        d = RSA.mod_inverse(e, phi)  # Call mod_inverse as a static method
+        return (n, e), (n, d)
 
-# Decrypt with private key
-def rsa_decrypt(private_key, ciphertext):
-    n, d = private_key
-    plaintext = pow(ciphertext, d, n)
-    return plaintext
+    def rsa_encrypt(self, plaintext):
+        n, e = self.public_key  # Access public_key as an instance attribute
+        cipher = pow(plaintext, e, n)
+        return cipher
+
+    def rsa_decrypt(self, ciphertext):
+        n, d = self.private_key  # Access private_key as an instance attribute
+        plaintext = pow(ciphertext, d, n)
+        return plaintext
 
 if __name__ == "__main__":
-    public_key, private_key = rsa_keygen(bits=2048)
+    rsa = RSA(bits=2048)
     plaintext = input("Enter plaintext: ")
     plaintext = int.from_bytes(plaintext.encode(), byteorder='big')
 
-    ciphertext = rsa_encrypt(public_key, plaintext)
-    decrypted = rsa_decrypt(private_key, ciphertext)
+    ciphertext = rsa.rsa_encrypt(plaintext)
+    decrypted = rsa.rsa_decrypt(ciphertext)
 
     decrypted_text = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, byteorder='big').decode()
 
-    print(f"Public Key (n, e): "+str(public_key))
-    print(f"Private Key (n, d): "+str(private_key))
-    print(f"Encrypted: "+str(ciphertext))
-    print(f"Decrypted: "+str(decrypted_text))
-
+    print(f"Public Key (n, e): {rsa.public_key}")
+    print(f"Private Key (n, d): {rsa.private_key}")
+    print(f"Encrypted: {ciphertext}")
+    print(f"Decrypted: {decrypted_text}")
